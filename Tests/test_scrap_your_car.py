b06837e0abe8065a1c6fbab22c57291618846fbc
Sachin Kumar Tiwari <tiwarisachin306@gmail.com>
2025-08-06 14:05:48 +0530

    Car_scrap_page

--- Tests/test_scrap_your_car.py
+++ Tests/test_scrap_your_car.py
@@ -3,7 +3,7 @@ import time
 
 import pytest
 from Utils.locators import Locators
-from Utils.selenium_helpers import safe_click, safe_send_keys
+from Utils.selenium_helpers import safe_click, safe_send_keys, safe_get_text
 
 test_data_path = r'C:\Users\Sachin Kumar Tiwari\PycharmProjects\Car_Bike\Data\test_data.json'
 with open(test_data_path) as f:
@@ -18,3 +18,5 @@ def test_sell_car(browserInstance):
     safe_click(browserInstance, Locators.car_service_main)
     safe_click(browserInstance, Locators.scrap_your_car)
     time.sleep(10)
+    print(safe_get_text(browserInstance,Locators.scrap_your_car_steps))
+    print(safe_get_text(browserInstance,Locators.why_choose_car))
